def34887e32081be5404b3d43a943b8b97265326
thiagobulhosa <71668999+thiagobulhosa@users.noreply.github.com>
2020-11-05 16:33:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,25 +5,39 @@
 #include "coletor.h"
 
 int main(){
+    int *a = (int*) malloc (sizeof(int));
+    int *b = (int*) malloc (sizeof(int));
+    *a=50;
+    *b=60;
+    char *c = (char*) malloc (sizeof(char));
+    char *d = (char*) malloc (sizeof(char));
+    *c='a';
+    *d='b';
+
 	Memoria *v=NULL, *w=NULL, *z=NULL, *y=NULL;
 
 	v=malloc2(sizeof(Memoria));
-	atrib(v, 10);
+	atrib(v, *a);
 
 	w=malloc2(sizeof(Memoria));
-	atrib(w, 20);
+	atrib(w, *b);
 
 	z=malloc2(sizeof(Memoria));
-	atrib(z,67);
+	atrib(z, *c);
 
 	y=malloc2(sizeof(Memoria));
-	atrib(y,203);
+	atrib(y, *d);
 
 	atribui2(&v,&w);
-	atribui2(&y,&z);
+	atribui2(&z,&y);
 
+	///Imprime area de memoria e numero de referencias
 	Imprime_memoria(v);
 	Imprime_memoria(w);
 	Imprime_memoria(y);
 	Imprime_memoria(z);
+
+	///Demonstracao conteudo
+	printf ("\nImpressao conteudo: %c %c\n", z->valor , y->valor);
+	printf ("Impressao conteudo: %d %d\n", v->valor, w->valor );
 }
